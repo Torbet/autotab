76baf8cb0ac527b1782a30713abeabc735fceabb
FelixHoare <felixphoare@outlook.com>
2025-03-11 17:35:16 +0000

    string fret tokeniser

--- tokenizer.py
+++ tokenizer.py
@@ -5,12 +5,9 @@ import gzip
 import urllib.request
 
 
-valid_times = [1, 2, 4, 8, 12, 16, 24, 32, 64]
-
-
 def tokenizer(track):
-  # with open(track, 'r') as file:
-  #   track = json.load(file)
+  with open(track, 'r') as file:
+    track = json.load(file)
 
   tokens = []
   bars = track['measures']
@@ -21,63 +18,63 @@ def tokenizer(track):
       notes = beat[x].get('notes', [])
       duration = beat[x].get('duration', None)
 
-      if beat[x].get('rest', False):
-        if duration:
-          num = duration[0]
-          den = duration[1]
-          dur = round_time(num, den)
-          tokens.append((i + 1, f'<R><T{dur}>'))
-
-      else:
-        note_tokens = []
-        if len(notes) > 1:
-          note_tokens.append('<C>')
-        for note in range(len(notes)):
-          fret = notes[note].get('fret', None)
-          string = notes[note].get('string', None)
-          tie = notes[note].get('tie', None)
-          bend = notes[note].get('bend', None)
-          hp = notes[note].get('hp', None)
-          if fret is not None and string is not None and duration:
-            fret_str = f'F{fret}'
-            str_str = f'S{string + 1}'
-            note_tokens.append(f'<{str_str}><{fret_str}>')
-            # if tie is not None:
-            #   note_tokens.append('<TI>')
-            if bend is not None:
-              note_tokens.append('<B>')
-            if hp is not None:
-              if x < len(beat) - 1:
-                next_note = beat[x + 1]['notes'][0]
-              elif i < len(bars) - 1:
-                next_note = bars[i + 1]['voices'][0]['beats'][0]['notes'][0]
-              else:
-                next_note = beat[x]['notes'][0]
-
-              if 'fret' in next_note and fret < next_note['fret']:
-                note_tokens.append('<H>')
-              else:
-                note_tokens.append('<P>')
-
-        if note_tokens and duration:
-          num = duration[0]
-          den = duration[1]
-          dur = round_time(num, den)
-          combined_note_token = ''.join(note_tokens) + f'<T{dur}>'
-
-          # if beat[x].get('letRing', False):
-          #   combined_note_token += '<LR>'
-          # if beat[x].get('upStroke', False):
-          #   combined_note_token += '<US>'
-          if beat[x].get('slide', False):
-            # combined_note_token += f'<SL{beat[x]["slide"]}>'
-            combined_note_token += '<SL>'
-          if len(notes) > 1:
-            combined_note_token += '</C>'
-
-          tokens.append((i + 1, combined_note_token))
+      # if beat[x].get('rest', False):
+      #   if duration:
+      #     num = duration[0]
+      #     den = duration[1]
+      #     dur = round_time(num, den)
+      #     tokens.append((i + 1, f'<R><T{dur}>'))
+      note_tokens = []
+      if len(notes) > 1:
+        note_tokens.append('<C>')
+      for note in range(len(notes)):
+        fret = notes[note].get('fret', None)
+        string = notes[note].get('string', None)
+        # tie = notes[note].get('tie', None)
+        # bend = notes[note].get('bend', None)
+        # hp = notes[note].get('hp', None)
+        if fret is not None and string is not None:
+          fret_str = f'F{fret}'
+          str_str = f'S{string + 1}'
+          note_tokens.append(f'<{str_str}><{fret_str}>')
+      if len(notes) > 1:
+        note_tokens.append('</C>')
+      tokens.append(''.join(note_tokens))
 
   return tokens
+          # if tie is not None:
+          #   note_tokens.append('<TI>')
+          # if bend is not None:
+          #   note_tokens.append('<B>')
+          # if hp is not None:
+          #   if x < len(beat) - 1:
+          #     next_note = beat[x + 1]['notes'][0]
+          #   elif i < len(bars) - 1:
+          #     next_note = bars[i + 1]['voices'][0]['beats'][0]['notes'][0]
+          #   else:
+          #     next_note = beat[x]['notes'][0]
+
+          #   if 'fret' in next_note and fret < next_note['fret']:
+          #     note_tokens.append('<H>')
+          #   else:
+          #     note_tokens.append('<P>')
+
+      # if note_tokens and duration:
+      #   num = duration[0]
+      #   den = duration[1]
+      #   dur = round_time(num, den)
+      #   combined_note_token = ''.join(note_tokens) + f'<T{dur}>'
+
+        # if beat[x].get('letRing', False):
+        #   combined_note_token += '<LR>'
+        # if beat[x].get('upStroke', False):
+        #   combined_note_token += '<US>'
+        # if beat[x].get('slide', False):
+        #   # combined_note_token += f'<SL{beat[x]["slide"]}>'
+        #   combined_note_token += '<SL>'
+        # if len(notes) > 1:
+
+        # tokens.append((i + 1, combined_note_token))
 
 
 def encoder():
@@ -129,25 +126,25 @@ def validate_tokens_in_vocab(enc, tokens):
   return list(unknown_tokens)
 
 
-def round_time(x, y):
-  value = x / y
+# def round_time(x, y):
+#   value = x / y
 
-  best_match = min(valid_times, key=lambda d: abs(value - (1 / d)))
+#   best_match = min(valid_times, key=lambda d: abs(value - (1 / d)))
 
-  return best_match
+#   return best_match
 
 
 # path = 'data/tabs_jsons/1100_2.json'
 
-# path = 'data/songsterr-data/Superman_0.json'
+path = 'data/songsterr-data/Superman_0.json'
 
-tokenurl = 'https://dqsljvtekg760.cloudfront.net/103/1017529/v3-5-24-ipkd1DcEtxBtNp23/0.json'
+# tokenurl = 'https://dqsljvtekg760.cloudfront.net/103/1017529/v3-5-24-ipkd1DcEtxBtNp23/0.json'
 
-r = requests.get(tokenurl)
+# r = requests.get(tokenurl)
 
-tokens = tokenizer(r.json())
+# tokens = tokenizer(r.json())
 
-# tokens = tokenizer(tab_dict)
+tokens = tokenizer(path)
 # tokens = [t for _, t in tokens]
 
 # # # tokens = tokenizer(tab_dict)
@@ -155,5 +152,5 @@ tokens = tokenizer(r.json())
 # # # #print(len(tokens))
 # enc = encoder()
 # validate_tokens_in_vocab(enc, tokens)
-# for t in tokens:
-#   print(t)
+for t in tokens:
+  print(t)
